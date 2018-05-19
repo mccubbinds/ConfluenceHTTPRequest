558301939cf4055040c1cc7d3bc72b65c1efd857
Derek McCubbin <derek.mccubbin@gmail.com>
2018-05-19 14:47:27 -0400

    Initial Commit

---
+++ ConfluenceJsonRequest/Confluence Json Request Form.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ConfluenceJsonRequest
+{
+    partial class ConfluenceJsonRequest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.logTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(71, 11);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(127, 20);
+            this.usernameTextbox.TabIndex = 0;
+            this.usernameTextbox.Text = "dmccubbin";
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(71, 37);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(127, 20);
+            this.passwordTextbox.TabIndex = 1;
+            this.passwordTextbox.Text = "bvtc8Cum!";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 40);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(12, 15);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(206, 11);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(52, 46);
+            this.GoButton.TabIndex = 4;
+            this.GoButton.Text = "GO";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // logTextbox
+            // 
+            this.logTextbox.Location = new System.Drawing.Point(15, 63);
+            this.logTextbox.Multiline = true;
+            this.logTextbox.Name = "logTextbox";
+            this.logTextbox.Size = new System.Drawing.Size(243, 118);
+            this.logTextbox.TabIndex = 5;
+            // 
+            // ConfluenceJsonRequest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 193);
+            this.Controls.Add(this.logTextbox);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Name = "ConfluenceJsonRequest";
+            this.Text = "Confluence Json Request Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.TextBox logTextbox;
+    }
+}
+
---
+++ ConfluenceJsonRequest/Confluence Json Request Form.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConfluenceJsonRequest
+{
+    public partial class ConfluenceJsonRequest : Form
+    {
+        public ConfluenceJsonRequest()
+        {
+            InitializeComponent();
+        }
+
+        JObject Get(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            string usernamePassword = usernameTextbox.Text + ":" + passwordTextbox.Text;
+
+            request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(usernamePassword));
+            request.PreAuthenticate = true;
+            try
+            {
+                WebResponse response = request.GetResponse();
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    string jsonObjectString = reader.ReadToEnd();
+
+                    return JObject.Parse(jsonObjectString);
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+
+                    MessageBox.Show(errorText);
+                }
+                throw;
+            }
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            JObject jsonObject = Get("https://syn-confluence.tms-orbcomm.com:8800/rest/api/content?spaceKey=GT1020FW&title=Device+Specification&expand=body.view");
+            
+            string tablesHtml = (string)jsonObject["results"][0]["body"]["view"]["value"];
+            logTextbox.AppendText(tablesHtml);
+        }
+    }
+}
